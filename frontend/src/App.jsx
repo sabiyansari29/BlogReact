bd64e6ebd5209975d880f6d596a29bf827725a8a
Sabiya29 <sabiya7398@gmail.com>
2026-06-17 14:26:32 +0530

    initial build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>___AI Blogs___</h1>
+      <ul>
+        <li>OpenAI Blog</li>
+        <li>Google DeepMind BlogOpenAI Blog</li>
+        <li>Anthropic News & Research</li>
+        <li>Hugging Face Blog</li>
+        <li>Towards AI</li>
+        <li>Machine Learning Mastery</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
