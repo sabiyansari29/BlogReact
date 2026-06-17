9b84a9989f9ab3695987bb913e311a9a5581b370
Sabiya29 <sabiya7398@gmail.com>
2026-06-17 15:20:13 +0530

    create contact form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,26 @@ const App = () => {
         <li>Hugging Face Blog</li>
         <li>Towards AI</li>
         <li>Machine Learning Mastery</li>
-      </ul>
-    </div>
+      </ul><br />
+      <h2>Registration Form</h2>
+
+      <form>
+        <label>Username:</label><br />
+        <input type="text" name="username" placeholder="Enter your username" /><br /><br />
+
+        <label>Email:</label><br />
+        <input type="email" name="email" placeholder="Enter your email" /><br /><br />
+
+        <label>Password:</label><br />
+        <input type="password" name="password" placeholder="Enter your password" /><br /><br />
+
+        <label>Message:</label><br />
+        <textarea name="message" rows="4" cols="30" placeholder="Write your message"></textarea><br /><br />
+
+        <button type="submit">Submit</button>
+      </form>
+
+    </div> 
   );
 }
 
